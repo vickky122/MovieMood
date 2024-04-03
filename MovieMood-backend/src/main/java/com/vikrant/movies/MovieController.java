f7eb4474168068cdf78a1ba45703f0d49c8793ef
Vikrant Yadav <vikrantmani45@gmail.com>
2024-04-03 20:22:34 +0530

    commit

--- MovieMood-backend/src/main/java/com/vikrant/movies/MovieController.java
+++ MovieMood-backend/src/main/java/com/vikrant/movies/MovieController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("/api/v1/movies")
 public class MovieController {
 
     @Autowired
